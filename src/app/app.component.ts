241ac492c2d374b67de998c5a6b39719788346ff
[DEadMan10sds] <54693096+DEadMan10sds@users.noreply.github.com>
2022-10-11 23:15:14 -0500

    ADD: Connection to firebase ADD: POST to firebase ADD: Get of firebase only console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,36 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit()
+  {
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    this.http.post(
+      'https://angularmaximilianhttpsection-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe(
+      responseData => {
+        console.log(responseData);
+      }
+    );
     console.log(postData);
   }
 
   onFetchPosts() {
     // Send Http request
+    this.http.get('https://angularmaximilianhttpsection-default-rtdb.firebaseio.com/posts.json').subscribe(
+      posts => {
+        console.log(posts)
+      }
+    );
   }
 
   onClearPosts() {
