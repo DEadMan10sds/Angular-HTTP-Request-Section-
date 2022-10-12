8b30c65688af51445ff33f9b0c68da65ce28e3f8
[DEadMan10sds] <54693096+DEadMan10sds@users.noreply.github.com>
2022-10-11 23:29:34 -0500

    ADD: Post interface (template of data) ADD: Get request uses pipe to convert fetch data into Post structure data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import {map} from 'rxjs/operators';
+import { Post } from './post.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit {
     this.onFetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http.post(
       'https://angularmaximilianhttpsection-default-rtdb.firebaseio.com/posts.json',
@@ -32,8 +33,23 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
-    this.http.get('https://angularmaximilianhttpsection-default-rtdb.firebaseio.com/posts.json').subscribe(
-      posts => {
+    this.http.get('https://angularmaximilianhttpsection-default-rtdb.firebaseio.com/posts.json')
+    .pipe(
+      map(
+        (responseData: {[key: string]: Post}) => {//Las llaves de arreglos definen un placeholder, de esta manera decimos que habrá un atributo de nombre cambiante (que llamaremos key) de tipo string
+          const postsArray: Post[] = [];
+          for(let key in responseData)
+          {
+            if(responseData.hasOwnProperty(key)) //Si el arreglo de responseData tiene una propiedad con el valor de 'key' (que es el iterador actual) hace push
+              postsArray.push({...responseData[key], id: key})
+          }
+          return postsArray;
+        }
+      )
+    )
+    .subscribe(
+      (posts: Post[]) => {
+        this.loadedPosts = posts;
         console.log(posts)
       }
     );
